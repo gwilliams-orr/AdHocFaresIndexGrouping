cae561e4c6fabf0088ea01ed5a71c040386fc283
Greg Williams <greggwilliams58@hotmail.com>
2019-06-18 14:54:02 +0100

    change of grouping
    Stress test of ['Carrier TOC / Third Party Code','Route Code','Origin Code','Destination Code','Product Code','Regulated_Status','Category','sector']

--- AdHocFaresIndexGrouping/AdHocFaresIndexGrouping.py
+++ AdHocFaresIndexGrouping/AdHocFaresIndexGrouping.py
@@ -12,7 +12,7 @@ def main():
     print("getting superfile for weights")
     rawsuperfile = pd.read_csv(superfilelocation + superfilename,
                                dtype={'Carrier TOC / Third Party Code':'category','Origin Code':'category','Destination Code':'category','Route Code':'category',
-                                      'Product Code':'category','Product Primary Code':'category','class':'category','sector':'category','ticket_type':'category'}
+                                      'Product Code':'category','Product Primary Code':'category','class':'category','sector':'category','ticket_type':'category','Category':'category'}
                                )
 
 
@@ -30,10 +30,13 @@ def main():
     # the filtered data is being grouped by the fields sector and carrier TOC, with the fields Earings and journeys being summed
     print("now grouping and summing the data")
     
-    rawsuperfile = rawsuperfile.groupby(['class'])['Adjusted Earnings Amount','Operating Journeys'].agg('sum')
+    groupedsuperfile = rawsuperfile.groupby(['Carrier TOC / Third Party Code','Route Code','Origin Code','Destination Code','Product Code','Regulated_Status','Category','sector'],observed=True)['Adjusted Earnings Amount','Operating Journeys'].agg('sum')
     
+    #remove aggregations where earnings/journeys are null
+    nonagroupedsuperfile = groupedsuperfile.dropna()
+
     #the filtered, grouped and summed data is then exported as a csv file using the imported module sharedfunctions
-    exportfile(rawsuperfile,outputto, "test agg of superfile")
+    exportfile(nonagroupedsuperfile,outputto, "test agg of superfile")
 
 
 #standard boilerplate to point compiler to start point of program.
